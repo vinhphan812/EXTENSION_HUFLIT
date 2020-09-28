d551147abccc1a662038fc85fadc14c35c3e1a41
vinhphan812 <vinhphan812@gmail.com>
2020-09-28 20:50:48 +0700

    Split name

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,7 +23,7 @@ function isDone(data){
      xhr.removeEventListener('readystatechange', DOMLogin);
      
      //DOM username
-     Menu.innerHTML = '<div class="user flex"><img src="assets/img/user.png"><p>' + data + '</p></div><ul class="menu flex">' + liMenu.join('') + '</ul>';
+     Menu.innerHTML = '<div class="user flex"><img src="assets/img/user.png"><p>' + data.split(' |')[1] + '</p></div><ul class="menu flex">' + liMenu.join('') + '</ul>';
      //DOM menu list
      document.getElementById('Schedules').addEventListener('click', Schedule);
      document.getElementById('Password').addEventListener('click', ChangePassword);
@@ -33,7 +33,7 @@ function isDone(data){
      //transition css
      setTimeout(function(){
           Menu.style.opacity = 1;
-          DOM.style.height = '330px';
+          DOM.style.height = '300px';
           DOM.style.opacity = 0;
      }, 450);
 
